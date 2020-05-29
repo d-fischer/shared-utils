838bfe3ec4f81c0a6951cc209264f7697343dfc2
Daniel Fischer <daniel@d-fischer.dev>
2020-05-29 17:26:04 +0200

    update eslint config again

--- src/decorators/NonEnumerable.ts
+++ src/decorators/NonEnumerable.ts
@@ -1,13 +1,13 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+// eslint-disable-next-line @typescript-eslint/no-explicit-any,@typescript-eslint/naming-convention
 export default function NonEnumerable(target: any, key: string) {
 	// first property defined in prototype, that's why we use getters/setters
 	// (otherwise assignment in object will override property in prototype)
 	Object.defineProperty(target, key, {
-		get: function() {
+		get: function () {
 			return;
 		},
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		set: function(this: any, val: any) {
+		set: function (this: any, val: any) {
 			// here we have a reference to the instance and can set property directly to it
 			Object.defineProperty(this, key, {
 				value: val,
