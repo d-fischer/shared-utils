3f5f0a81b0526e9135a2dc3a6bc7feaa9e26b3a9
Daniel Fischer <daniel@d-fischer.dev>
2020-07-09 13:24:24 +0200

    drop name from deprecate message

--- src/functions/deprecate/deprecateClass.ts
+++ src/functions/deprecate/deprecateClass.ts
@@ -1,14 +1,14 @@
 import { Constructor } from '../../types/constructor';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any,@typescript-eslint/naming-convention
-export function deprecateClass<T extends Constructor<{}>>(Base: T, msg: string): T {
+export function deprecateClass<T extends Constructor>(Base: T, msg: string): T {
 	let deprecateNoticeShown = false;
 	return class extends Base {
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		constructor(...args: any[]) {
 			if (!deprecateNoticeShown) {
 				// eslint-disable-next-line no-console
-				console.warn(`[deprecated] ${Base.name} - ${msg}`);
+				console.warn(`[deprecated] ${msg}`);
 				deprecateNoticeShown = true;
 			}
 			super(...args);
