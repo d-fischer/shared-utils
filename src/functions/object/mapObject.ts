3705bccbb36ab5775880225191ffc7e867fc8c15
Daniel Fischer <daniel@d-fischer.dev>
2020-12-14 21:15:15 +0100

    fix mapObject typing

--- src/functions/object/mapObject.ts
+++ src/functions/object/mapObject.ts
@@ -1,9 +1,9 @@
-import { ObjMapPart } from '../../types/object';
+import { ObjMap, ObjMapPart } from '../../types/object';
 
 export function mapObject<T, O, Obj = Record<string, T>>(
 	obj: Obj,
 	fn: (value: T, key: Extract<keyof Obj, string>) => O
-) {
+): ObjMap<Obj, O> {
 	const mapped = Object.entries(obj).map(
 		// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
 		([key, value]: [Extract<keyof Obj, string>, T]) => ({ [key]: fn(value, key) } as ObjMapPart<Obj, O>)
