f723762ac906220e4d8fe3f172eddb41e5bd6be9
Daniel Fischer <daniel@d-fischer.dev>
2023-01-01 21:44:14 +0100

    fix new compiler errors

--- src/functions/object/forEachObjectEntry.ts
+++ src/functions/object/forEachObjectEntry.ts
@@ -1,3 +1,6 @@
-export function forEachObjectEntry<T, Obj>(obj: Obj, fn: (value: T, key: keyof Obj) => void): void {
-	Object.entries(obj).forEach(([key, value]) => fn(value, key as keyof Obj));
+export function forEachObjectEntry<T, Obj extends Record<string, unknown>>(
+	obj: Obj,
+	fn: (value: T, key: keyof Obj) => void
+): void {
+	(Object.entries(obj) as Array<[string, T]>).forEach(([key, value]) => fn(value, key as keyof Obj));
 }
--- src/functions/object/mapObject.ts
+++ src/functions/object/mapObject.ts
@@ -1,12 +1,12 @@
 import type { ObjMap, ObjMapPart } from '../../types/object';
 
-export function mapObject<T, O, Obj = Record<string, T>>(
+export function mapObject<T, O, Obj extends Record<string, unknown> = Record<string, T>>(
 	obj: Obj,
 	fn: (value: T, key: Extract<keyof Obj, string>) => O
 ): ObjMap<Obj, O> {
-	const mapped = Object.entries(obj).map(
+	const mapped = (Object.entries(obj) as Array<[string, T]>).map(
 		// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-		([key, value]: [string, T]) => ({ [key]: fn(value, key as Extract<keyof Obj, string>) } as ObjMapPart<Obj, O>)
+		([key, value]) => ({ [key]: fn(value, key as Extract<keyof Obj, string>) } as ObjMapPart<Obj, O>)
 	);
 	return Object.assign({}, ...mapped) as ObjMap<Obj, O>;
 }
